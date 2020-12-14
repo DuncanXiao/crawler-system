aad57dc3a759975c7abbff468b34e8db43ee9752
duxiao <duxiao@gz-duxiaombp.intranet.theknot.com>
2020-12-14 18:13:49 +0800

    wip

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
-import proxy.verify
-import damai.session
+import scrapy
 
-if __name__ == '__main__':
-  ip = '60.217.155.211:8060'
-  # res = proxy.verify.iPVerify('60.217.155.211:8060')
-  # print(res)
-  # if(res):
-  damai.session.login(ip)
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]').getall():
+            print('--------')
+            print(quote.xpath('/span[@class="text"]/text()').get())
+            # yield {
+            #     'text': quote.xpath('/span[@class="text"]/text()').get(),
+            #     'author': quote.xpath('/span/small[@class="author"]/text()').get(),
+            # }
+        # print(response.xpath('//div[@class="quote"]/span[@class="text"]/text()').get())
+        # print('sssss----======')
+        # print(response.css('div.quote'))
+        # print('--------')
+        # next_page = response.css('li.next a::attr("href")').get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, self.parse)
